a580655928fc86beb3469227ddca57449c9deec9
Az-r-ow <antoine.azar123@hotmail.com>
2021-10-26 12:48:45 +0200

    Added feature : Checking a user's balance by tagging

--- commands/text/balance.js
+++ commands/text/balance.js
@@ -2,13 +2,32 @@ const User = require('../../db_connection.js');
 module.exports = {
   name: 'balance',
   description: 'Check your balance',
-  async execute(interaction, client){
-    await User.findOne({id: interaction.member.id}).then(user_data => {
+  async execute(interaction, args, client){
+
+    const {userMention} = require('@discordjs/builders')
+
+    const user_id = args.length > 1 && args[1].match(/\d{18}/g) ? args[1].match(/\d{18}/g)[0] : interaction.member.id;
+
+    let username;
+
+    const user = await interaction.guild.members.fetch(user_id).then(user =>{
+      username = user.user. username;
+    });
+
+
+    await User.findOne({id: user_id}).then(user_data => {
       //No user found
       // Send an empty balance
-      if(!user_data)return interaction.reply('Your balance is \`0\`');
-      
-      interaction.reply(`Your balance is : \`${user_data.balance}\``);
+      if(!user_data)return interaction.reply({
+        embeds: [{
+          fields: [{name: `${username}\'s Balance :`, value: '\`0\`'}]
+        }]
+      });
+
+      interaction.reply({embeds: [{
+        fields: [{name: `${username}\'s Balance :`, value: `\`${user_data.balance}\`` }]
+      }]
+    });
     })
   }
 }
